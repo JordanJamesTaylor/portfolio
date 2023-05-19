1d8048ba5456e63f18b66fb05bb2a651afc06c54
JordanJamesTaylor <jordjamestaylor@gmail.com>
2023-05-19 18:54:10 -0400

    set page sections and smooth scrolling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useRef } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+import Home from "./components/Home"
+import About from "./components/About";
+import Projects from "./components/Projects";
+
+import "./App.css";
+
+export default function App() {
+
+  // init useRef hook
+  const home = useRef(null);
+  const about = useRef(null);
+  const projects = useRef(null);
+  const contact = useRef(null);
+
+  // grab window element and scroll to component
+  const scrollToSection = (elelmentRef) => {
+    window.scrollTo({
+        // scroll to top of ref element
+        top: elelmentRef.current.offsetTop,
+        // scroll effect
+        behavior: "smooth",
+      }
     );
   }
-}
-
-export default App;
+  
+  return (
+    <div className="App">
+      <div className="navbar">
+        <ul id="navbar">
+          <li onClick={() => scrollToSection(home)} className="links">Home</li>
+          <li onClick={() => scrollToSection(about)} className="links">About</li>
+          <li onClick={() => scrollToSection(projects)} className="links">Projects</li>
+          <li onClick={() => scrollToSection(contact)} className="links">Contact Me</li>
+        </ul>
+      </div>
+      <div ref={home} className="home">
+        <h2>HOME</h2>
+      </div>
+      <div ref={about} className="about">
+        <h2>ABOUT</h2>
+      </div>
+      <div ref={projects} className="projects">
+        <h2>PROJECTS</h2>
+      </div>
+      <div ref={contact} className="contact">
+        <h2>CONTACT</h2>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Projects.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Projects() {
+    return (
+      <h1>PROJECTS COMPONENT</h1>
+    ); 
+};
