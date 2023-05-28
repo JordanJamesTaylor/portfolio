05df0026016f547b9fc0771361cd647b9885c1e5
JordanJamesTaylor <jordjamestaylor@gmail.com>
2023-05-28 19:09:55 -0400

    added typewriter effect

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,11 +1,26 @@
 import { Fragment } from 'react';
+import Typewriter from 'typewriter-effect';
 
 import './about.css';
 
 export default function About() {
 return (
   <Fragment>
-    <h1 className='name' data-cursor-text='Hello there!'>Hi, I'm Jordan Taylor</h1>
+    <div className='bio-title'>
+      <h1 className='name' data-cursor-text='Hello there!'>Hi, I'm Jordan Taylor</h1>
+      <div className='type-writer'>
+      <div className="type-writer">
+      <Typewriter
+          options={{
+            strings: ['Software Developer', 'Full-Stack Developer', 'Your Next Developer'],
+            autoStart: true,
+            
+            loop: true,
+          }}
+      />
+    </div>
+      </div>
+    </div>
     <div className='bio'>
     <p>
      Lorem ipsum dolor sit amet consectetur. Leo pulvinar morbi molestie ut nunc eu. Massa elit amet a orci imperdiet. In tincidunt urna lectus quis faucibus varius ac commodo. Integer ut tellus donec ipsum ac ornare tellus.
